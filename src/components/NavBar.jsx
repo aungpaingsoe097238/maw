c9b14b3a0a4decd190cfdca38ab50bab3fe0e436
Aung Paing Soe <aungpaingsoe097238@gmail.com>
2023-05-29 22:13:53 +0630

    nav bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      {/* nav bar */}
+      <nav className=" w-full bg-gray-100 text-slate-700 p-[10px] ">
+        <ul className=" w-[70%] mx-auto flex justify-between ">
+          <li className="">
+            <a href="" className="">
+              Maw Lay
+            </a>
+          </li>
+          <li className="">
+            <a href="" className="">
+              Home
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {/* nav bar */}
+    </>
+  );
+};
+
+export default NavBar;
