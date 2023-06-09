59440198356ee0e138285b5f78c6ad524b904df0
Aung Paing Soe <aungpaingsoe097238@gmail.com>
2023-06-09 23:09:36 +0630

    project prop

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,28 @@
-import React from "react";
-import top from "../assets/img/top.png";
+import React, { useState } from "react";
+import ProjectCard from "./ProjectCard";
 
 const Portfolio = () => {
+  const [projects, setProjects] = useState([
+    {
+      title: "Project One",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio vitae enim error nisi debitis ad est, fugiat nam. Nostrum",
+      githubUrl: "https://github.com/khaingzarnimaw",
+      url: "https://www.google.com",
+    },
+    {
+      title: "Project Two",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio vitae enim error nisi debitis ad est, fugiat nam. Nostrum",
+      githubUrl: "https://github.com/khaingzarnimaw",
+      url: "https://www.google.com",
+    },
+    {
+      title: "Project Three",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio vitae enim error nisi debitis ad est, fugiat nam. Nostrum",
+      githubUrl: "https://github.com/khaingzarnimaw",
+      url: "https://www.google.com",
+    },
+  ]);
+
   return (
     <div className=" w-[70%] mx-auto ">
       <div className=" mb-5">
@@ -10,30 +31,11 @@ const Portfolio = () => {
       </div>
 
       <div className=" flex gap-3">
-        <div className=" basis-1/3">
-          <img src={top} alt="" />
-
-          <div className=" flex justify-between mt-2">
-            <span className=" font-bold">Alpha</span>
-            <span>Icons</span>
-          </div>
-
-          <div className=" my-3 text-slate-700 " >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio vitae
-            enim error nisi debitis ad est, fugiat nam. Nostrum voluptatum
-            repellat enim quo eveniet ab tempore odit blanditiis consequatur
-            accusantium.
-          </div>
-
-          <div className=" flex gap-2 text-xs ">
-            <div className=" bg-slate-200 px-3 py-1 ">Javascript</div>
-            <div className=" bg-slate-200 px-3 py-1 ">Reactjs</div>
-            <div className=" bg-slate-200 px-3 py-1 ">tailwindcss</div>
-          </div>
-        </div>
-
-        <div className=" basis-1/3">Project Two</div>
-        <div className=" basis-1/3">Project Three</div>
+        {
+          projects?.map((project,index)=>{
+            return <div className=" basis-1/3 " key={index}> <ProjectCard data={project} /> </div>
+          })
+        }
       </div>
     </div>
   );
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import top from "../assets/img/top.png";
+import { AiFillGithub, AiOutlineArrowRight } from "react-icons/ai";
+
+function ProjectCard({ data }) {
+  return (
+    <div>
+      <img src={top} alt="" />
+
+      <div className=" flex justify-between items-center mt-2">
+        <span className=" font-bold">{data.title}</span>
+        <span className=" flex ">
+          <a href={ data.githubUrl } target="_black" >
+            <AiFillGithub className=" text-3xl cursor-pointer hover:text-slate-500" />
+          </a>
+          <a href={ data.url } target="_black">
+            <AiOutlineArrowRight className=" text-3xl cursor-pointer hover:text-slate-500" />
+          </a>
+        </span>
+      </div>
+
+      <div className=" my-3 text-slate-700 ">{data.text}</div>
+
+      <div className=" flex gap-2 text-xs ">
+        <div className=" bg-slate-200 px-3 py-1 ">Javascript</div>
+        <div className=" bg-slate-200 px-3 py-1 ">Reactjs</div>
+        <div className=" bg-slate-200 px-3 py-1 ">tailwindcss</div>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
