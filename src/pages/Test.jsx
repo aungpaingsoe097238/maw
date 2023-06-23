e87dad0e9d89272fa5d0db42b61ddbdc5a253207
Aung Paing Soe <aungpaingsoe097238@gmail.com>
2023-06-23 22:38:18 +0630

    Responsive with media query to Top

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 const Test = () => {
   return (
-    <div className=" relative h-screen ">
+    <>
+      {/* sm for mobile */}
+      {/* md for tablet */}
+      {/* lg for laptop */}
+      {/* xl for desktop */}
+      <div className="h-screen flex justify-center lg:items-center">
+        
+        <div>Hello World!</div>
 
-      <div className=" w-[100px] h-[100px] bg-red-500 absolute left-[100px] top-[35%] "></div>
-
-    </div>
+      </div>
+    </>
   );
 };
 
